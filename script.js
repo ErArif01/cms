e899414c44addde2ea013772f44ebc69888f6f4c
ER MOHD ARIF <141546622+ErArif01@users.noreply.github.com>
2024-01-10 14:26:14 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+
+$(document).ready(function () {
+    function loadContent(page) {
+        $.getJSON(`data/${page}.json`, function (data) {
+            const template = $('#template').html();
+            const compiledTemplate = Handlebars.compile(template);
+            const html = compiledTemplate(data);
+            $('#content').html(html);
+        });
+    }
+
+    // Initial load
+    const currentPage = window.location.pathname.split('/').pop().split('.')[0];
+    loadContent(currentPage);
+
+    // Navigation click event
+    $('nav a').click(function (e) {
+        e.preventDefault();
+        const page = $(this).attr('href').split('.').shift();
+        loadContent(page);
+    });
+});
